c7f18ba658557c384fdc15f75aae7b1a596ecdb0
JosephB-Workstation <JEBloxsom@student.fullsail.edu>
2020-05-12 01:20:30 -0400

    Fundementals - 5/11/2020
    <Work started 5/11/2020>
    
    Little functionality implemented, tonight I was working out tool bars and implementing buttons on them. I will continue my work on them tomorrow.

--- app/src/main/java/com/check/app/MainActivity.java
+++ app/src/main/java/com/check/app/MainActivity.java
@@ -1,8 +1,12 @@
 package com.check.app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.mainToolBar);
+        setSupportActionBar(toolbar);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mainactivity_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.addList:
+                //code here for add list activity.
+                return true;
+            case R.id.logOut:
+                //code here for logging out when  firebase support is added
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
